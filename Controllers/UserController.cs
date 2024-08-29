87de5cd6dedcea3db54baa64cb5699cd66d39db6
Godsgift Oghenechohwo <godsgiftoghenechohwo@gmail.com>
2024-08-29 17:36:06 +0100

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using ConsumeApiExample.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumeApiExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class UserController : ControllerBase
+    {
+        private readonly IReqresService _reqresService;
+
+        public UserController(IReqresService reqresService)
+        {
+            _reqresService = reqresService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _reqresService.GetUserByIdAysnc(id);
+            if (user == null)
+            {
+                return NoContent();
+            }
+            return Ok(user);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var users = await _reqresService.GetUsers();
+            return Ok(users);
+        }
+    }
+}
---
+++ Services/IReqresService.cs
@@ -0,0 +1,10 @@
+﻿using ConsumeApiExample.DTO;
+
+namespace ConsumeApiExample.Services
+{
+    public interface IReqresService
+    {
+        Task<UserDTO> GetUserByIdAysnc(int id);
+        Task<UsersDTO> GetUsers();
+    }
+}
